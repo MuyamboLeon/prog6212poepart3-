9abf0d3c5daf18f7f8504e180c6ec7599f29cf3d
MuyamboLeon <st10372746@imconnect.edu.za>
2025-11-21 13:02:58 +0200

    Added a report view for the academic manager
    implemented a review page for the manager, so they can see all the reports in the system from part 2

--- obj/Debug/net9.0/The CMCS.AssemblyInfo.cs
+++ obj/Debug/net9.0/The CMCS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The CMCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7689db2bede154f93791ba5dda88bd9748303eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13eb90ff728f807777fbfe0e8353057a5a52f9e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("The CMCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The CMCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
